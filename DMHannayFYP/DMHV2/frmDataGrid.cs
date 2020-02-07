3daab048261ef0c401242803deef7522561c1dd1
Darren Hannay <21702155@bucks.ac.uk>
2020-02-07 13:39:21 +0000

    Added the start of DataGridViewer Added controls to the interface of the data grid

--- DMHannayFYP/DMHV2/frmDataGrid.cs
+++ DMHannayFYP/DMHV2/frmDataGrid.cs
@@ -12,9 +12,53 @@ namespace DMHV2
 {
     public partial class frmDataGrid : Form
     {
+        public int UserID;
         public frmDataGrid()
         {
             InitializeComponent();
         }
+
+        private void TsbNew_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TsbRecord_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TsbDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TsbRefresh_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TsbPrint_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TsbFind_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TsbClose_Click(object sender, EventArgs e)
+        {
+            this.Close();   // close the data grid view screen
+        }
+
+        private void frmDataGrid_Load(object sender, EventArgs e)
+        {
+
+        }
+        #region DataLoading
+
+        #endregion
     }
 }

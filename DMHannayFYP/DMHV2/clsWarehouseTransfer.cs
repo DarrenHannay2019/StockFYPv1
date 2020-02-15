490ccc2411670ea4e8df52010e311dafabced415
Darren Hannay <21702155@bucks.ac.uk>
2020-02-15 13:32:08 +0000

    added Warehouse Transfer class outline to application added database outline

---
+++ DMHannayFYP/DMHV2/clsWarehouseTransfer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMHV2
+{
+    public class clsWarehouseTransfer : clsUtils
+    {
+    }
+    public class clsWarehouseTransferHead : clsWarehouseTransfer
+    {
+
+    }
+    public class clsWarehouseTransferLine : clsWarehouseTransfer
+    {
+
+    }
+}

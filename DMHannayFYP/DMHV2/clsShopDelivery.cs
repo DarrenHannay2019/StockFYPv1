be6a2145dc73347a4b68ffb92819b022dfaca4be
DarrenHannay2019 <21702155@bucks.ac.uk>
2020-02-15 13:54:10 +0000

    clsshopdelivery class

---
+++ DMHannayFYP/DMHV2/clsShopDelivery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMHV2
+{
+    public class clsShopDelivery : clsUtils
+    {
+    }
+    public class clsShopDeliveryHead : clsShopDelivery
+    { }
+    public class clsShopDeliveryLine : clsShopDelivery
+    {
+
+    }
+}

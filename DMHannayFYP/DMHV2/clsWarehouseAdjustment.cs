b9c5a4692bde61cceebee381b04bda8ad895a840
Darren Hannay <21702155@bucks.ac.uk>
2020-02-15 13:29:10 +0000

    added WarehouseAdjustment base class also added outline classes for the database

---
+++ DMHannayFYP/DMHV2/clsWarehouseAdjustment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMHV2
+{
+    public class clsWarehouseAdjustment : clsUtils
+    {
+
+    }
+    public class clsWarehouseAdjustmentHead : clsWarehouseAdjustment
+    {
+
+    }
+    public class clsWarehouseAdjustmentLine : clsWarehouseAdjustment
+    {
+
+    }
+}

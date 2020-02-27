6f68ecf40aa39b9d84e5b8f2c061a9e94e823b84
DarrenHannay2019 <dmhsoftad01@outlook.com>
2020-02-27 06:28:04 +0000

    Warehouse Returns Saving Updating Logic

--- DMHannayFYP/DMHV2/clsUtils.cs
+++ DMHannayFYP/DMHV2/clsUtils.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-
-namespace DMHV2
+﻿namespace DMHV2
 {
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Text.RegularExpressions;
+
     public class clsUtils
     {
         // Global properties
--- DMHannayFYP/DMHV2/clsWarehouseReturn.cs
+++ DMHannayFYP/DMHV2/clsWarehouseReturn.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,14 +15,62 @@ namespace DMHV2
     {
         public bool SaveWarehouseReturnHead()
         {
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = GetConnString(1);
+                using (SqlCommand InsertCmd = new SqlCommand())
+                {
+                    InsertCmd.Connection = conn;
+                    InsertCmd.Connection.Open();
+                    InsertCmd.CommandType = CommandType.Text;
+                    InsertCmd.CommandText = "INSERT INTO tblWHReturns (ToWarehouseRef, WarehouseRef, Reference, TotalItems, TransactionDate, CreatedBy, CreatedDate) VALUES (@ToWarehouseRef, @WarehouseRef, @Reference, @TotalItems, @TransactionDate, @CreatedBy, @CreatedDate)";
+                    InsertCmd.Parameters.AddWithValue("@ToWarehouseRef", SupplierRef);
+                    InsertCmd.Parameters.AddWithValue("@WarehouseRef", WarehouseRef);
+                    InsertCmd.Parameters.AddWithValue("@Reference", Reference);
+                    InsertCmd.Parameters.AddWithValue("@TotalItems", TotalItems);
+                    InsertCmd.Parameters.AddWithValue("@TransactionDate", MovementDate);
+                    InsertCmd.Parameters.AddWithValue("@CreatedBy", UserID);
+                    InsertCmd.Parameters.AddWithValue("@CreatedDate", DateTime.Now);
+                    InsertCmd.ExecuteNonQuery();
+                }
+            }
             return SaveToDB;
         }
         public bool UpdateWarehouseReturnHead()
         {
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = GetConnString(1);
+                using (SqlCommand UpdateCmd = new SqlCommand())
+                {
+                    UpdateCmd.Connection = conn;
+                    UpdateCmd.Connection.Open();
+                    UpdateCmd.CommandType = CommandType.Text;
+                    UpdateCmd.CommandText = "UPDATE tblWHReturns SET ToWarehouseRef = @ToWarehouseRef, WarehouseRef = @WarehouseRef, Reference = @Reference, TotalItems = @TotalItems, TransactionDate = @TransactionDate WHERE ReturnsID = @ReturnsID";
+                    UpdateCmd.Parameters.AddWithValue("@ReturnsID", ID);
+                    UpdateCmd.Parameters.AddWithValue("@WarehouseRef", WarehouseRef);
+                    UpdateCmd.Parameters.AddWithValue("@ToWarehouseRef", SupplierRef);
+                    UpdateCmd.Parameters.AddWithValue("@Reference", Reference);
+                    UpdateCmd.Parameters.AddWithValue("@TotalItems", TotalItems);
+                    UpdateCmd.Parameters.AddWithValue("@TransactionDate", MovementDate);
+                    UpdateCmd.ExecuteNonQuery();
+                }
+            }
             return UpdateToDB;
         }
         public bool DeleteWarehouseReturnHead()
         {
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = GetConnString(1);
+                using (SqlCommand DeleteCmd = new SqlCommand())
+                {
+                    DeleteCmd.Connection = conn;
+                    DeleteCmd.Connection.Open();
+                    DeleteCmd.CommandType = CommandType.Text;
+                    DeleteCmd.CommandText = "";
+                }
+            }
             return DeleteFromDB;
         }
     }
@@ -28,14 +78,57 @@ namespace DMHV2
     {
         public bool SaveWarehouseReturnLine()
         {
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = GetConnString(1);
+                using (SqlCommand InsertCmd = new SqlCommand())
+                {
+                    InsertCmd.Connection = conn;
+                    InsertCmd.Connection.Open();
+                    InsertCmd.CommandType = CommandType.Text;
+                    InsertCmd.CommandText = "INSERT INTO tblWHReturnLines (ReturnID, StockCode, Qty, Value) VALUES (@ReturnID, @StockCode, @Qty, @Value)";
+                    InsertCmd.Parameters.AddWithValue("@ReturnID", ID);
+                    InsertCmd.Parameters.AddWithValue("@StockCode", StockCode);
+                    InsertCmd.Parameters.AddWithValue("@Qty", Qty);
+                    InsertCmd.Parameters.AddWithValue("@Value", Value);
+                    InsertCmd.ExecuteNonQuery();
+                }
+            }
             return SaveToDB;
         }
         public bool UpdateWarehouseReturnLine()
         {
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = GetConnString(1);
+                using (SqlCommand UpdateCmd = new SqlCommand())
+                {
+                    UpdateCmd.Connection = conn;
+                    UpdateCmd.Connection.Open();
+                    UpdateCmd.CommandType = CommandType.Text;
+                    UpdateCmd.CommandText = "UPDATE tblWHReturnLines SET Qty = @Qty,Value = @Value WHERE ReturnID = @ReturnID AND StockCode = @StockCode";
+                    UpdateCmd.Parameters.AddWithValue("@ReturnID", ID);
+                    UpdateCmd.Parameters.AddWithValue("@StockCode", StockCode);
+                    UpdateCmd.Parameters.AddWithValue("@Qty", Qty);
+                    UpdateCmd.Parameters.AddWithValue("@Value", Value);
+                    UpdateCmd.ExecuteNonQuery();
+                }
+            }
             return UpdateToDB;
         }
         public bool DeleteWarehouseReturnLine()
         {
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = GetConnString(1);
+                using (SqlCommand DeleteCmd = new SqlCommand())
+                {
+                    DeleteCmd.Connection = conn;
+                    DeleteCmd.Connection.Open();
+                    DeleteCmd.CommandType = CommandType.Text;
+                    DeleteCmd.CommandText = "";
+                }
+            }
             return DeleteFromDB;
         }
     }

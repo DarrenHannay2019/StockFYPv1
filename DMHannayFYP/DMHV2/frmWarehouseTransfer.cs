53353c1949954a4c3fe663aebf76799e89c79577
DarrenHannay2019 <dmhsoftad01@outlook.com>
2020-02-27 09:38:14 +0000

    frmWarehouseTranfers added with controls No customisation to warehouse from shops No Code

---
+++ DMHannayFYP/DMHV2/frmWarehouseTransfer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DMHV2
+{
+    public partial class frmWarehouseTransfer : Form
+    {
+        public frmWarehouseTransfer()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdAddToGrid_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CmdDeleteFromGrid_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CmdOK_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CmdCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CmdClear_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

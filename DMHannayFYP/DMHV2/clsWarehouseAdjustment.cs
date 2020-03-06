9f9e340aeac1e3cf51ed5141cbd03fbeb6135541
DarrenHannay2019 <dmhsoftad01@outlook.com>
2020-03-06 13:00:44 +0000

    Error Checking in clsWarehouseAdjustments Done

--- DMHannayFYP/DMHV2/clsWarehouseAdjustment.cs
+++ DMHannayFYP/DMHV2/clsWarehouseAdjustment.cs
@@ -25,16 +25,32 @@
         public int GetLastWarehouseAdjustmentHead()
         {
             Result = 0;
-            using (SqlConnection conn = new SqlConnection())
+            try
             {
-                conn.ConnectionString = GetConnString(1);
-                conn.Open();
-                using (SqlCommand SelectCmd = new SqlCommand())
+                using (SqlConnection conn = new SqlConnection())
                 {
-                    SelectCmd.Connection = conn;
-                    SelectCmd.CommandText = "SELECT COUNT(*) AS MaxRef FROM tblWarehouseAdjustments";
-                    Result = (int)SelectCmd.ExecuteScalar();
+                    conn.ConnectionString = GetConnString(1);
+                    try
+                    {
+                        conn.Open();
+                        using (SqlCommand SelectCmd = new SqlCommand())
+                        {
+                            SelectCmd.Connection = conn;
+                            SelectCmd.CommandText = "SELECT COUNT(*) AS MaxRef FROM tblWarehouseAdjustments";
+                            Result = (int)SelectCmd.ExecuteScalar();
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        System.Windows.Forms.MessageBox.Show("Error in Saving\n" + ex.Message);
+                        throw;
+                    }                    
                 }
+            }
+            catch (SqlException ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Error in Saving\n" + ex.Message);
+                throw;
             }
             return Result;
         }
@@ -43,64 +59,142 @@
     {
         public bool SaveWarehouseAdjustmentHead()
         {
-            using (SqlConnection conn = new SqlConnection())
+            try
             {
-                conn.ConnectionString = GetConnString(1);
-                using (SqlCommand InsertCmd = new SqlCommand())
+                using (SqlConnection conn = new SqlConnection())
                 {
-                    InsertCmd.Connection = conn;
-                    InsertCmd.Connection.Open();
-                    InsertCmd.CommandType = CommandType.Text;
-                    InsertCmd.CommandText = "INSERT INTO tblWarehouseAdjustments(WarehouseRef, Reference, TotalLossItems, TotalGainItems, MovementDate, CreatedBy, CreatedDate) VALUES (@WarehouseRef, @Reference, @TotalLossItems, @TotalGainItems, @MovementDate, @CreatedBy, @CreatedDate)";
-                    InsertCmd.Parameters.AddWithValue("@WarehouseRef", WarehouseRef);
-                    InsertCmd.Parameters.AddWithValue("@Reference", Reference);
-                    InsertCmd.Parameters.AddWithValue("@TotalLossItems", TotalLossItems);
-                    InsertCmd.Parameters.AddWithValue("@TotalGainItems", TotalGainItems);
-                    InsertCmd.Parameters.AddWithValue("@MovementDate", MovementDate);
-                    InsertCmd.Parameters.AddWithValue("@CreatedBy", UserID);
-                    InsertCmd.Parameters.AddWithValue("@CreatedDate", DateTime.Now);
-                    InsertCmd.ExecuteNonQuery();
+                    conn.ConnectionString = GetConnString(1);
+                    try
+                    {
+                        using (SqlCommand InsertCmd = new SqlCommand())
+                        {
+                            InsertCmd.Connection = conn;
+                            InsertCmd.Connection.Open();
+                            InsertCmd.CommandType = CommandType.Text;
+                            InsertCmd.CommandText = "INSERT INTO tblWarehouseAdjustments(WarehouseRef, Reference, TotalLossItems, TotalGainItems, MovementDate, CreatedBy, CreatedDate) VALUES (@WarehouseRef, @Reference, @TotalLossItems, @TotalGainItems, @MovementDate, @CreatedBy, @CreatedDate)";
+                            InsertCmd.Parameters.AddWithValue("@WarehouseRef", WarehouseRef);
+                            InsertCmd.Parameters.AddWithValue("@Reference", Reference);
+                            InsertCmd.Parameters.AddWithValue("@TotalLossItems", TotalLossItems);
+                            InsertCmd.Parameters.AddWithValue("@TotalGainItems", TotalGainItems);
+                            InsertCmd.Parameters.AddWithValue("@MovementDate", MovementDate);
+                            InsertCmd.Parameters.AddWithValue("@CreatedBy", UserID);
+                            InsertCmd.Parameters.AddWithValue("@CreatedDate", DateTime.Now);
+                            Result = (int)InsertCmd.ExecuteNonQuery();
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        SaveToDB = false;
+                        System.Windows.Forms.MessageBox.Show("Error in Saving\n" + ex.Message);
+                        throw;
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
                 }
             }
+            catch (SqlException ex)
+            {
+                SaveToDB = false;
+                System.Windows.Forms.MessageBox.Show("Error in Saving\n" + ex.Message);
+                throw;
+            }
+            if (Result == 1)
+                SaveToDB = true;
+            else
+                SaveToDB = false;
             return SaveToDB;
         }
         public bool UpdateWarehouseAdjustmentHead()
         {
-            using (SqlConnection conn = new SqlConnection())
+            try
             {
-                conn.ConnectionString = GetConnString(1);
-                using (SqlCommand UpdateCmd = new SqlCommand())
+                using (SqlConnection conn = new SqlConnection())
                 {
-                    UpdateCmd.Connection = conn;
-                    UpdateCmd.Connection.Open();
-                    UpdateCmd.CommandType = CommandType.Text;
-                    UpdateCmd.CommandText = "UPDATE tblWarehouseAdjustments SET WarehouseRef = @WarehouseRef, Reference = @Reference, TotalLossItems = @TotalLossItems, TotalGainItems = @TotalGainItems, MovementDate = @MovementDate WHERE ID = @ID";
-                    UpdateCmd.Parameters.AddWithValue("@ID", ID);
-                    UpdateCmd.Parameters.AddWithValue("@WarehouseRef", ShopRef);
-                    UpdateCmd.Parameters.AddWithValue("@Reference", Reference);
-                    UpdateCmd.Parameters.AddWithValue("@TotalLossItems", TotalLossItems);
-                    UpdateCmd.Parameters.AddWithValue("@TotalGainItems", TotalGainItems);
-                    UpdateCmd.Parameters.AddWithValue("@MovementDate", MovementDate);
-                    UpdateCmd.ExecuteNonQuery();
+                    conn.ConnectionString = GetConnString(1);
+                    try
+                    {
+                        using (SqlCommand UpdateCmd = new SqlCommand())
+                        {
+                            UpdateCmd.Connection = conn;
+                            UpdateCmd.Connection.Open();
+                            UpdateCmd.CommandType = CommandType.Text;
+                            UpdateCmd.CommandText = "UPDATE tblWarehouseAdjustments SET WarehouseRef = @WarehouseRef, Reference = @Reference, TotalLossItems = @TotalLossItems, TotalGainItems = @TotalGainItems, MovementDate = @MovementDate WHERE WarehouseAdjustmentID = @WarehouseAdjustmentID";
+                            UpdateCmd.Parameters.AddWithValue("@WarehouseAdjustmentID", ID);
+                            UpdateCmd.Parameters.AddWithValue("@WarehouseRef", ShopRef);
+                            UpdateCmd.Parameters.AddWithValue("@Reference", Reference);
+                            UpdateCmd.Parameters.AddWithValue("@TotalLossItems", TotalLossItems);
+                            UpdateCmd.Parameters.AddWithValue("@TotalGainItems", TotalGainItems);
+                            UpdateCmd.Parameters.AddWithValue("@MovementDate", MovementDate);
+                            UpdateCmd.ExecuteNonQuery();
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        UpdateToDB = false;
+                        System.Windows.Forms.MessageBox.Show("Error in Saving\n" + ex.Message);
+                        throw;
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
                 }
             }
+            catch (SqlException ex)
+            {
+                UpdateToDB = false;
+                System.Windows.Forms.MessageBox.Show("Error in Saving\n" + ex.Message);
+                throw;
+            }            
+            if (Result == 1)
+                UpdateToDB = true;
+            else
+                UpdateToDB = false;
             return UpdateToDB;
         }
         public bool DeleteWarehouseAdjustmentHead()
         {
-            using (SqlConnection conn = new SqlConnection())
+            try
             {
-                conn.ConnectionString = GetConnString(1);
-                using (SqlCommand DeleteCmd = new SqlCommand())
+                using (SqlConnection conn = new SqlConnection())
                 {
-                    DeleteCmd.Connection = conn;
-                    DeleteCmd.Connection.Open();
-                    DeleteCmd.CommandType = CommandType.Text;
-                    DeleteCmd.CommandText = "DELETE from tblWarehouseAdjustments where ID = @ID;";
-                    DeleteCmd.Parameters.AddWithValue("@ID", ID);
-                    DeleteCmd.ExecuteNonQuery();
+                    conn.ConnectionString = GetConnString(1);
+                    try
+                    {
+                        using (SqlCommand DeleteCmd = new SqlCommand())
+                        {
+                            DeleteCmd.Connection = conn;
+                            DeleteCmd.Connection.Open();
+                            DeleteCmd.CommandType = CommandType.Text;
+                            DeleteCmd.CommandText = "DELETE from tblWarehouseAdjustments where WarehouseAdjustmentID = @WarehouseAdjustmentID;";
+                            DeleteCmd.Parameters.AddWithValue("@WarehouseAdjustmentID", ID);
+                            Result = (int)DeleteCmd.ExecuteNonQuery();
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        System.Windows.Forms.MessageBox.Show("Error in Saving\n" + ex.Message);
+                        DeleteFromDB = false;
+                        throw;
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
                 }
             }
+            catch (SqlException ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Error in Saving\n" + ex.Message);
+                DeleteFromDB = false;
+                throw;
+            }            
+            if (Result == 1)
+                DeleteFromDB = true;
+            else
+                DeleteFromDB = false;
             return DeleteFromDB;
         }
     }
@@ -108,61 +202,136 @@
     {
         public bool SaveWarehouseAdjustmentLine()
         {
-            using (SqlConnection conn = new SqlConnection())
+            try
             {
-                conn.ConnectionString = GetConnString(1);
-                using (SqlCommand InsertCmd = new SqlCommand())
+                using (SqlConnection conn = new SqlConnection())
                 {
-                    InsertCmd.Connection = conn;
-                    InsertCmd.Connection.Open();
-                    InsertCmd.CommandType = CommandType.Text;
-                    InsertCmd.CommandText = "INSERT INTO tblWarehouseAdjustmentsLines (WarehouseAdjustID, StockCode, MovementType, Qty, Value) VALUES (@WarehouseAdjustID, @StockCode, @MovementType, @Qty, @Value)";
-                    InsertCmd.Parameters.AddWithValue("@WarehouseAdjustID", ID);
-                    InsertCmd.Parameters.AddWithValue("@StockCode", StockCode);
-                    InsertCmd.Parameters.AddWithValue("@MovementType", MovementType);
-                    InsertCmd.Parameters.AddWithValue("@Qty", Qty);
-                    InsertCmd.Parameters.AddWithValue("@Value", Value);
-                    InsertCmd.ExecuteNonQuery();
-                }
+                    conn.ConnectionString = GetConnString(1);
+                    try
+                    {
+                        using (SqlCommand InsertCmd = new SqlCommand())
+                        {
+                            InsertCmd.Connection = conn;
+                            InsertCmd.Connection.Open();
+                            InsertCmd.CommandType = CommandType.Text;
+                            InsertCmd.CommandText = "INSERT INTO tblWarehouseAdjustmentsLines (WarehouseAdjustID, StockCode, MovementType, Qty, Value) VALUES (@WarehouseAdjustID, @StockCode, @MovementType, @Qty, @Value)";
+                            InsertCmd.Parameters.AddWithValue("@WarehouseAdjustID", ID);
+                            InsertCmd.Parameters.AddWithValue("@StockCode", StockCode);
+                            InsertCmd.Parameters.AddWithValue("@MovementType", MovementType);
+                            InsertCmd.Parameters.AddWithValue("@Qty", Qty);
+                            InsertCmd.Parameters.AddWithValue("@Value", Value);
+                            Result = (int)InsertCmd.ExecuteNonQuery();
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        System.Windows.Forms.MessageBox.Show("Error in Saving\n" + ex.Message);
+                        throw;
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
+                } 
             }
+            catch (SqlException ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Error in Saving\n" + ex.Message);
+                throw;
+            }
+            if (Result == 1)
+                SaveToDB = true;
+            else
+                SaveToDB = false;
             return SaveToDB;
         }
         public bool UpdateWarehouseAdjustmentLine()
         {
-            using (SqlConnection conn = new SqlConnection())
+            try
             {
-                conn.ConnectionString = GetConnString(1);
-                using (SqlCommand UpdateCmd = new SqlCommand())
+                using (SqlConnection conn = new SqlConnection())
                 {
-                    UpdateCmd.Connection = conn;
-                    UpdateCmd.Connection.Open();
-                    UpdateCmd.CommandType = CommandType.Text;
-                    UpdateCmd.CommandText = "UPDATE tblWarehouseAdjustmentsLines SET MovementType = @MovementType, Qty = @Qty, Value = @Value WHERE WarehouseAdjustID = @WarehouseAdjustID AND StockCode = @StockCode";
-                    UpdateCmd.Parameters.AddWithValue("@StockCode", StockCode);
-                    UpdateCmd.Parameters.AddWithValue("@MovementType", MovementType);
-                    UpdateCmd.Parameters.AddWithValue("@Qty", Qty);
-                    UpdateCmd.Parameters.AddWithValue("@Value", Value);
-                    UpdateCmd.Parameters.AddWithValue("@WarehouseAdjustID", ID);
-                    UpdateCmd.ExecuteNonQuery();
+                    conn.ConnectionString = GetConnString(1);
+                    try
+                    {
+                        using (SqlCommand UpdateCmd = new SqlCommand())
+                        {
+                            UpdateCmd.Connection = conn;
+                            UpdateCmd.Connection.Open();
+                            UpdateCmd.CommandType = CommandType.Text;
+                            UpdateCmd.CommandText = "UPDATE tblWarehouseAdjustmentsLines SET MovementType = @MovementType, Qty = @Qty, Value = @Value WHERE WarehouseAdjustID = @WarehouseAdjustID AND StockCode = @StockCode";
+                            UpdateCmd.Parameters.AddWithValue("@StockCode", StockCode);
+                            UpdateCmd.Parameters.AddWithValue("@MovementType", MovementType);
+                            UpdateCmd.Parameters.AddWithValue("@Qty", Qty);
+                            UpdateCmd.Parameters.AddWithValue("@Value", Value);
+                            UpdateCmd.Parameters.AddWithValue("@WarehouseAdjustID", ID);
+                            Result = (int)UpdateCmd.ExecuteNonQuery();
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        System.Windows.Forms.MessageBox.Show("Error in Saving\n" + ex.Message);
+                        UpdateToDB = false;
+                        throw;
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }                    
                 }
             }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Error in Saving\n" + ex.Message);
+                throw;
+            }            
+            if (Result == 1)
+                UpdateToDB = true;
+            else
+                UpdateToDB = false;
             return UpdateToDB;
         }
         public bool DeleteWarehouseAdjustmentLine()
         {
-            using (SqlConnection conn = new SqlConnection())
+            try
             {
-                conn.ConnectionString = GetConnString(1);
-                using (SqlCommand DeleteCmd = new SqlCommand())
+                using (SqlConnection conn = new SqlConnection())
                 {
-                    DeleteCmd.Connection = conn;
-                    DeleteCmd.Connection.Open();
-                    DeleteCmd.CommandType = CommandType.Text;
-                    DeleteCmd.CommandText = "DELETE from tblWarehouseAdjustmentsLines WHERE WHAdjustID = @WHAdjustID;";
-                    DeleteCmd.Parameters.AddWithValue("@WHAdjustID", ID);
-                    DeleteCmd.ExecuteNonQuery();
+                    conn.ConnectionString = GetConnString(1);
+                    try
+                    {
+                        using (SqlCommand DeleteCmd = new SqlCommand())
+                        {
+                            DeleteCmd.Connection = conn;
+                            DeleteCmd.Connection.Open();
+                            DeleteCmd.CommandType = CommandType.Text;
+                            DeleteCmd.CommandText = "DELETE from tblWarehouseAdjustmentsLines WHERE WarehouseAdjustmentID = @WarehouseAdjustmentID;";
+                            DeleteCmd.Parameters.AddWithValue("@WHAdjustID", ID);
+                            DeleteCmd.ExecuteNonQuery();
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        DeleteFromDB = false;
+                        System.Windows.Forms.MessageBox.Show("Error in Saving\n" + ex.Message);
+                        throw;
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
                 }
             }
+            catch (SqlException ex)
+            {
+                DeleteFromDB = false;
+                System.Windows.Forms.MessageBox.Show("Error in Saving\n" + ex.Message);
+                throw;
+            }            
+            if (Result == 1)
+                DeleteFromDB = true;
+            else
+                DeleteFromDB = false;           
             return DeleteFromDB;
         }
     }

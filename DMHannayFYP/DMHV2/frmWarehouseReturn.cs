6a19e7f4dc1a625fd8dd842e3042d5febe364de4
DarrenHannay2019 <dmhsoftad01@outlook.com>
2020-04-09 21:03:30 +0100

    txtStockCode_Leave  Warehouse Returns Completed

--- DMHannayFYP/DMHV2/frmWarehouseReturn.cs
+++ DMHannayFYP/DMHV2/frmWarehouseReturn.cs
@@ -99,7 +99,11 @@
 
         private void txtStockCode_Leave(object sender, EventArgs e)
         {
-
+            txtStockCode.Text = clsUtils.ChangeCase(txtStockCode.Text, 1);
+            clsStock stock = new clsStock();
+            stock.StockCode = txtStockCode.Text.TrimEnd();
+            stock.SupplierRef = txtWarehouseRef.Text.TrimEnd();
+            txtCurrentQty.Text = stock.GetWarehouseStockQty().ToString();
         }
 
         private void txtWarehouseRef_Leave(object sender, EventArgs e)
